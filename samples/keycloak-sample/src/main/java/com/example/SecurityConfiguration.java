762d3a401689766037553ea01446f543efcc86ee
Carl J. Mosca <carljmosca@gmail.com>
2017-09-22 11:15:45 -0400

    added logout configuration which is not currently supported in library

--- samples/keycloak-sample/src/main/java/com/example/SecurityConfiguration.java
+++ samples/keycloak-sample/src/main/java/com/example/SecurityConfiguration.java
@@ -14,37 +14,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
     @Value("${hostname.preferred}")
     String hostnamePreferred;
 
-	@Value("${security.saml2.metadata-url}")
-	String metadataUrl;
+    @Value("${security.saml2.metadata-url}")
+    String metadataUrl;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    if (hostnamePreferred == null || hostnamePreferred.isEmpty()) {
-	        hostnamePreferred = "localhost:8443";
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (hostnamePreferred == null || hostnamePreferred.isEmpty()) {
+            hostnamePreferred = "localhost:8443";
         }
-		http
-			.authorizeRequests()
-				.antMatchers("/saml/**").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.apply(saml())
-				.serviceProvider()
-					.keyStore()
-						.storeFilePath("saml/keystore.jks")
-						.password("secret")
-						.keyname("spring")
-						.keyPassword("secret")
-						.and()
-					.protocol("https")
-					.hostname(hostnamePreferred)
-					.basePath("/")
-					.and()
-				.identityProvider()
-					.metadataFilePath(metadataUrl)
-					.and();
-	}
+        http
+                .authorizeRequests()
+                .antMatchers("/saml/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .apply(saml())
+                .serviceProvider()
+                .keyStore()
+                .storeFilePath("saml/keystore.jks")
+                .password("secret")
+                .keyname("spring")
+                .keyPassword("secret")
+                .and()
+                .protocol("https")
+                .hostname(hostnamePreferred)
+                .basePath("/")
+                .and()
+                .identityProvider()
+                .metadataFilePath(metadataUrl)
+                .and()
+                .and()
+                .logout();
+              
+    }
 }
--- samples/keycloak-sample/src/main/java/com/example/controller/HelloController.java
+++ samples/keycloak-sample/src/main/java/com/example/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.controller;
 
 import com.example.data.HelloResponse;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
 import org.opensaml.saml2.core.impl.NameIDImpl;
 import org.springframework.security.providers.ExpiringUsernameAuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 @Controller
 @RequestMapping("/hello")
@@ -18,18 +26,55 @@ public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody
     HelloResponse sayHello(HttpServletRequest request,
-                           @RequestParam(value = "name", required = false, defaultValue = "tomcat") String name) {
+            @RequestParam(value = "name", required = false, defaultValue = "tomcat") String name) {
 
         HelloResponse response = new HelloResponse();
         String username = "";
         if (request.getUserPrincipal() instanceof ExpiringUsernameAuthenticationToken) {
-            ExpiringUsernameAuthenticationToken token = (ExpiringUsernameAuthenticationToken)request.getUserPrincipal();
-            NameIDImpl nameId = (NameIDImpl)token.getPrincipal();
+            ExpiringUsernameAuthenticationToken token = (ExpiringUsernameAuthenticationToken) request.getUserPrincipal();
+            NameIDImpl nameId = (NameIDImpl) token.getPrincipal();
             username = nameId.getValue();
         }
         request.getUserPrincipal().getName();
-        response.setMessage(String.format("Name: %s User Principal: %s",name, username));
+        response.setMessage(String.format("Name: %s User Principal: %s", name, username));
         return response;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public @ResponseBody
+    String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+//        HttpSession session = request.getSession(false);
+//        SecurityContextHolder.clearContext();
+//        session = request.getSession(false);
+//        if (session != null) {
+//            session.invalidate();
+//        }
+//        
+        try {
+            request.logout();
+        } catch (ServletException ex) {
+            Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        try {
+//            //request.getSession().invalidate();
+//            request.logout();
+//            for (Cookie cookie : request.getCookies()) {
+//                if ("jsessionid".equalsIgnoreCase(cookie.getName())) {
+//                    response.addCookie(cookie);       
+//                    response.sendRedirect("/saml/SSO?GLO=true");
+//                }
+//            }
+//        } catch (ServletException ex) {
+//            Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(HelloController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+        return "";
     }
 
 }
